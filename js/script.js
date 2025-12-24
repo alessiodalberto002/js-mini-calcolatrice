0b9789133985922274380db79aa7bff55c49d2aa
alessiodalberto002 <aledalberto002.com>
2025-12-24 14:58:39 +0100

    Recuperiamo gli elementi dal Dom

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// RECUPERO DEGLI ELEMENTI DAL DOM //
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const risultato = document.getElementById("risultato");
