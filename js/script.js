93464d4d387e05ac3ae09ccb4321b507f9b0a594
alessiodalberto002 <aledalberto002.com>
2025-12-24 15:00:09 +0100

    Creo una funzione per ottenere i numeri e controllare che siano validi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 const num1Input = document.getElementById("num1");
 const num2Input = document.getElementById("num2");
 const risultato = document.getElementById("risultato");
+
+// CREIAMO UNA FUNZIONE PER OTTENERE I NUMERI E CONTROLLARE CHE SIANO VALIDI //
+function ottieniNumeri() {
+    const num1 = Number(num1Input.value);
+    const num2 = Number(num2Input.value);
+
+    if (num1Input.value === "" || num2Input.value === "") {
+        risultato.textContent = "Inserisci entrambi i numeri";
+        return null;
+    }
+
+    return { num1, num2 };
+}
